c9fe1464167ef5f0396ee39f1bddc006afca4e19
Mert Yılmaz <mertylmz8134@gmail.com>
2024-04-18 20:17:44 +0300

    s

--- src/FindBook.UI/FindBook.UI.App/obj/Debug/net8.0/FindBook.UI.App.AssemblyInfo.cs
+++ src/FindBook.UI/FindBook.UI.App/obj/Debug/net8.0/FindBook.UI.App.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FindBook.UI.App")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+66bc7dd44125dd1f47d0fc3cc825ccdb9a153e8b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+79471681e604be8e557c984efcfc373f9e1ef5dc")]
 [assembly: System.Reflection.AssemblyProductAttribute("FindBook.UI.App")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FindBook.UI.App")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
